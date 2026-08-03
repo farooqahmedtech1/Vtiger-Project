c7ce6a7c57d3573e7d57b560aa20c2922fcd7e80
unknown <farooqahmed.tech1@gmail.com>
2026-08-03 08:13:33 +0530

    jenkins file added4

--- utils/utility.js
+++ utils/utility.js
@@ -1,10 +1,58 @@
+// export class Utility {
+
+//     constructor(page) {
+//         this.page = page;
+//     }
+
+//     async launchurl(url) {
+//         await this.page.goto(url);
+//     }
+
+//     async clickElement(locator) {
+//         await locator.click();
+//     }
+
+//     async fillText(locator, value) {
+//         await locator.fill(value);
+//     }
+
+//     async selectDropdown(locator, value) {
+//         await locator.selectOption({ value });
+//     }
+
+//     async popup(locator) {
+//         return await Promise.all([
+//             this.page.waitForEvent('popup'),
+//             locator.click()
+//         ]);
+//     }
+
+//     async hover(locator) {
+//         await locator.hover();
+//     }
+
+//     async selectDate(calendarIcon, month, year, date) {
+
+//     await calendarIcon.click();
+
+//     while (
+//         (await this.page.locator('//td[@class="title"]').textContent()).trim() !==
+//         `${month}, ${year}`
+//     ) {
+//         await this.page.locator('//td[@class="button nav"]').nth(2).click();
+//     }
+
+//     await this.page.locator(`//td[text()="${date}"]`).click();
+// }
+// }
+
 export class Utility {
 
     constructor(page) {
         this.page = page;
     }
 
-    async launchurl(url) {
+    async launchApplication(url) {
         await this.page.goto(url);
     }
 
@@ -32,16 +80,15 @@ export class Utility {
     }
 
     async selectDate(calendarIcon, month, year, date) {
+        await calendarIcon.click();
 
-    await calendarIcon.click();
+        while (
+            (await this.page.locator('//td[@class="title"]').textContent()).trim() !==
+            `${month}, ${year}`
+        ) {
+            await this.page.locator('//td[@class="button nav"]').last().click();
+        }
 
-    while (
-        (await this.page.locator('//td[@class="title"]').textContent()).trim() !==
-        `${month}, ${year}`
-    ) {
-        await this.page.locator('//td[@class="button nav"]').nth(2).click();
+        await this.page.locator(`//td[text()="${date}"]`).click();
     }
-
-    await this.page.locator(`//td[text()="${date}"]`).click();
-}
 }
